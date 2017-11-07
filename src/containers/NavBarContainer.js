5ba40404f83a9091bdb5ef81706f9e7255136548
Matthew Kitt <mk@mkitt.net>
2017-11-07 06:50:54 -0700

    Proper guard against back button on the root

--- src/containers/NavBarContainer.js
+++ src/containers/NavBarContainer.js
@@ -1,15 +1,15 @@
 // @flow
 import { withRouter } from 'react-router-dom'
-import { memoize } from 'ramda'
 import { compose, mapProps, pure } from 'recompose'
+import { BASE_URL } from '../constants/networking'
 import NavBar from '../components/modules/NavBar'
 
-const selectGoBack = memoize(props => (
-  props.location.pathname !== '/' ? props.history.goBack : null
-))
+const isRoot = ({ location }) => (
+  location.pathname === '/' || location.pathname === BASE_URL
+)
 
 const withProps = mapProps(props => ({
-  goBack: selectGoBack(props),
+  goBack: isRoot(props) ? null : props.history.goBack,
 }))
 
 export default compose(
