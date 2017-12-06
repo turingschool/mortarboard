b1eb803f29d4273a626ae8ed094e6ad8f7f5cb01
Matthew Kitt <mk@mkitt.net>
2017-12-06 05:01:29 -0700

    Remove the remaining probe logs

--- src/containers/NavBarContainer.js
+++ src/containers/NavBarContainer.js
@@ -2,7 +2,6 @@
 import { withRouter } from 'react-router-dom'
 import { anyPass, compose, equals, not, path } from 'ramda'
 import { branch, mapProps, pure, renderComponent } from 'recompose'
-import { log } from '../lib/utils'
 import { BASE_URL } from '../constants/networking'
 import NavBar, { Nav } from '../components/modules/NavBar'
 
@@ -25,10 +24,8 @@ const isAuthenticated = compose(
 )
 
 const withProps = mapProps(props => ({
-  displayName: 'NavBarContainer',
   isAuthenticated: isAuthenticated(props),
   goBack: isRoot(props) ? null : path(['history', 'goBack'], props),
-  BASE_URL,
 }))
 
 const hideIfNotAuthenticated = branch(
@@ -39,7 +36,6 @@ const hideIfNotAuthenticated = branch(
 export default compose(
   withRouter,
   withProps,
-  log,
   hideIfNotAuthenticated,
   pure,
 )(NavBar)
