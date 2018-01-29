5277e6acd8834cab01cd543a16cf4a160e585e3c
Matthew Kitt <mk@mkitt.net>
2018-01-29 05:37:33 -0700

    Adds the grid tool for aligning layouts

--- src/components/templates/Layout.js
+++ src/components/templates/Layout.js
@@ -1,22 +1,38 @@
 // @flow
 import React, { type Node } from 'react'
+import Gridzzly from 'gridzzly'
 import glamorous from 'glamorous'
 import NavBar from '../../containers/NavBarContainer'
+import { isDev } from '../../lib/utils'
 
 type Props = {
   children: Node,
 }
 
 export default (props: Props) => (
-  <div>
+  <View>
     <NavBar />
     <Main role="main">
       {props.children}
     </Main>
-  </div>
+    { isDev() &&
+      <Gridzzly
+        autoHide
+        persist
+        cycleKey="~"
+        position="fixed"
+        size={32}
+        toggleKey="`"
+        zIndex={666}
+      />
+    }
+  </View>
 )
 
 // -------------------------------------
+const View = glamorous.div({
+  position: 'relative',
+})
 
 const Main = glamorous.main({
   position: 'relative',
