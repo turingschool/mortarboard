a7a4a969da9a9ea6a9d96d00dc8d7821e0445cda
Matthew Kitt <mk@mkitt.net>
2018-04-18 07:02:42 -0600

    Freeze the style enums

--- src/constants/theme.js
+++ src/constants/theme.js
@@ -6,7 +6,7 @@ export const ANIMATE_ROTATE = css.keyframes('ANIMATE_ROTATE', {
   to: { transform: 'rotate(720deg)' },
 })
 
-export const COLORS = {
+export const COLORS = Object.freeze({
   BLUE: '#35c5fc',
   RED: '#f43306',
   MAGENTA: '#ff00ff',
@@ -19,20 +19,20 @@ export const COLORS = {
   GREY_E: '#e5e5e5',
   WHITE: '#fff',
   LIGHT_GREEN: '#8ad30e',
-}
+})
 // check for Black and white
 
-export const EASE = {
+export const EASE = Object.freeze({
   IN_OUT: 'cubic-bezier(0.23, 1, 0.32, 1)',
   IN_OUT_QUART: 'cubic-bezier(0.77, 0, 0.175, 1)',
   IN_OUT_CUBIC: 'cubic-bezier(0.645, 0.045, 0.355, 1)',
-}
+})
 
-export const MQ = {
+export const MQ = Object.freeze({
   MIN_BREAK_31: '@media (min-width: 31em)',
   MIN_BREAK_48: '@media (min-width: 48em)',
-}
+})
 
-export const ZZZ = {
+export const ZZZ = Object.freeze({
   MODAL: 1080,
-}
+})
